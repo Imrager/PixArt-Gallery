2b2335f84ef7e9a801abc63970cc14240dae1b27
Imrager <davidlclarke92@yahoo.com>
2019-05-20 11:55:58 -0400

    added create User

--- client/src/components/User.js
+++ client/src/components/User.js
@@ -1,19 +1,25 @@
 import React, { Component } from 'react';
 import axios from 'axios'
-import { Link } from 'react-router-dom'
+import { Redirect, Link } from 'react-router-dom'
 
 class User extends Component {
     state = {
         user: {
             name: ''
         },
-        images: []
+        images: [],
+        redirectToHome: false
     }
     getUser = () => {
         axios.get(`/api/${this.props.match.params.id}`).then(res => {
 
             this.setState({ user: res.data.userId })
             this.setState({ images: res.data.images })
+        })
+    }
+    deleteUser = () => {
+        axios.delete(`/api/${this.props.match.params.id}`).then(res => {
+            this.setState({redirectToHome: true})
         })
     }
     componentDidMount() {
@@ -23,15 +29,22 @@ class User extends Component {
         // const images = ()=>{this.state.images.map(image =>{
         //   return  image.name
         // })}
+        if(this.state.redirectToHome) {
+            return (< Redirect to="/" />)
+        }
         return (
+            
             <div>
+
                 <h1>{this.state.user.name}</h1>
                 {this.state.images.map(image => {
-                    return <img src={image.imageUrl}></img>
+                    let imgUrl = <img src={image.imageUrl}></img>
+                    let imgName = <h2>{image.name}</h2>
+                    return imgUrl
                 })}
-                {this.state.images.map(image => {
+                {/* {this.state.images.map(image => {
                     return image.name
-                })}
+                })} */}
                 <br></br>
                 <Link to='/'>Go to Home</Link>
             </div>
